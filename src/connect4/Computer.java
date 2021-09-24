5f01a2d75802494ca113bca76ac05fd1c955cf8e
SomeyaGinji <cist.b220.g.someya@gmail.com>
2021-09-25 00:02:09 +0900

    connect4を作成中

--- src/connect4/Computer.java
+++ src/connect4/Computer.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class Computer {
 
     Random random = new Random();
-    int[][] ascore = new int[10][7]; //10はdepth
 
     Computer(){
 
@@ -15,42 +14,116 @@ public class Computer {
         int i,j,decidej;
         Assessment assessment = new Assessment();
 
+        int tmparray[][] = new int[6][7]; //盤面の情報をtmparrayに記憶
+        for (int I=0;I<=5;I++){
+            for (int J=0;J<=6;J++){
+                tmparray[I][J] = array[I][J];
+            }
+        }
 
         decidej = random.nextInt(7); //0~6の7つの乱数
 
         //探索アルゴリズムが下に続く
         //盤面評価のクラスかメソッドが入るかも
-        for (j=0;j<=6;j++){
-            i=5;
-            while (i>=0){
-                if (array[i][j]==-1){
-                    array[i][j]=1;
+
+        int[] ascore = new int[7];
+        int[][] bscore = new int[7][7];
+        int[][][] cscore = new int[7][7][7];
+
+        for (int j1=0;j1<=6;j1++) {
+            int i1 = 5;
+            while (i1 >= 0) {
+                if (array[i1][j1] == -1) {
+                    array[i1][j1] = 1;
                     break;
                 }
-                i--;
-                if (i==-1){
-
+                i1--;
+                if (i1 == -1) {
                     break;
                 }
             }
 
-            ascore[1][j] = assessment.calcscore(i,j,array,score);
-            System.out.println(ascore[1][j]);
+            ascore[j1] = assessment.calcscore(i1, j1, array, score);
+            //System.out.print(ascore[j1]+"  ");
+
+            for (int j2=0;j2<=6;j2++){ //2手先（プレイヤーの番）を考える
+                int i2=5;
+                while (i2>=0){
+                    if (array[i2][j2]==-1){ //空の部分について、
+                        array[i2][j2]=0; //プレイヤー球が入ると仮定して
+                        break;
+                    }
+                    i2--;
+                    if (i2==-1){
+                        break;
+                    }
+                }
+
+                bscore[j1][j2] = assessment.calcscore(i2,j2,array,ascore[j1]);
+                //System.out.print(bscore[j1][j2]+" ");
+
+                for (int j3=0;j3<=6;j3++) {
+                    int i3 = 5;
+                    while (i3 >= 0) {
+                        if (array[i3][j3] == -1) {
+                            array[i3][j3] = 1;
+                            break;
+                        }
+                        i3--;
+                        if (i3 == -1) {
+                            break;
+                        }
+                    }
+
+                    cscore[j1][j2][j3] = assessment.calcscore(i3, j3, array, bscore[j1][j2]);
+                    System.out.print(cscore[j1][j2][j3] + " ");
+                    try {
+                        array[i3][j3]=-1;
+                    } catch (ArrayIndexOutOfBoundsException e){ }
+
+                }
+
+                try {
+                    array[i2][j2]=-1;
+                } catch (ArrayIndexOutOfBoundsException e){ }
+
+            }
 
             try {
+                array[i1][j1]=-1;
+            } catch (ArrayIndexOutOfBoundsException e){ }
+
+        }
+
+        //プレイヤー優勢度が一番低くなるものを探索して打つ手を決める
+        int minscore = bscore[0][0];
+        for (int j1=0;j1<=6;j1++) {
+            for (int j2=0;j2<=6;j2++) {
+                if (bscore[j1][j2] < minscore) {
+                    minscore = bscore[j1][j2];
+                    decidej = j1;
+                }
+            }
+        }
+
+            /*try {
                 array[i][j]=-1;
             } catch (ArrayIndexOutOfBoundsException e){
 
+            }*/
+
+        for (int I=0;I<=5;I++){
+            for (int J=0;J<=6;J++){
+                array[I][J] = tmparray[I][J];
             }
         }
 
-        int minscore = ascore[1][0];
-        for (j=0;j<=6;j++){
-            if (ascore[1][j]<minscore){
-                minscore = ascore[1][j];
+        /*for (j=0;j<=6;j++){
+            if (ascore[2][j]<minscore){
+                minscore = ascore[2][j];
                 decidej = j;
             }
-        }
+        }*/
 
         //どの手が最善か決める
         /*int max = assessment.valuearray[0][0];
@@ -61,8 +134,6 @@ public class Computer {
                 }
             }
         }*/
-
-
 
         // プレイヤーがリーチなら阻止する手を打つ
         int m,n,count;
